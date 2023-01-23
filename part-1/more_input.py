46c43633ef196606bd7676d345c5036bb331a7ce
vrothx00 <vrothx00@gmail.com>
2023-01-23 17:21:01 +0100

    Added assign Information from the user

---
+++ part-1/more_input.py
@@ -0,0 +1,8 @@
+# Write your solution here
+g_name = input("Given name: ")
+f_name = input("Family name: ")
+s_name = input("Street address: ")
+c_name = input("City and postal code: ")
+print(g_name + " " + f_name)
+print(s_name)
+print(c_name)
